c697ee00c14b57b94dda4038cdde9a7be1ce40af
VigneshMQuovantis <vignesh.m@quovantis.com>
2022-04-12 14:36:29 +0530

    [VIGNESH M] Added: Registration and Login

---
+++ BackendApplication/BookStoreApplication/CommonLayer/UserModels/UserEntities.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer.UserModels
+{
+    public class UserEntities
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId UserId { get; set; }
+        public string FullName { get; set; }
+
+        public string EmailId { get; set; }
+
+        public string Password { get; set; }
+    }
+}
